85cf2b2063d813b933f52fa8c434ac18c37e916a
Rachel <rachel@summa.one>
2018-09-17 17:30:04 -0700

    Add tweak to pubkey test.

--- pysecp256k1/tests/test_pysecp256k1.py
+++ pysecp256k1/tests/test_pysecp256k1.py
@@ -12,6 +12,7 @@ class TestPysecp256k1(unittest.TestCase):
         ]
         self.pubkey = bytes.fromhex('0250863ad64a87ae8a2fe83c1af1a8403cb53f53e486d8511dad8a04887e5b2352')   # noqa: E501
         self.uncomp_pubkey = bytes.fromhex('0479BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8')    # noqa: E501
+        self.tweak = bytes.fromhex('aca0338ffd29daeb82021b179348db68ad0740d66698264d2e69e1ae9ab007f1')  # noqa: E501
 
     def test_secp256k1_create_context(self):
 
@@ -88,14 +89,16 @@ class TestPysecp256k1(unittest.TestCase):
     def test_ec_pubkey_serialize(self):
         for flags in self.context_flags:
             # Create context
-            ctx = pysecp256k1.context_create(flags)
+            secp256k1_ctx = pysecp256k1.context_create(flags)
 
             # Create COMPRESSED secp256k1_pubkey object to serialize
-            secp256k1_pubkey = pysecp256k1.ec_pubkey_parse(ctx, self.pubkey)
+            secp256k1_pubkey = pysecp256k1.ec_pubkey_parse(
+                secp256k1_ctx,
+                self.pubkey)
 
             # Serialize COMPRESSED pubkey object to byte array pointer
             pubkey_ser = pysecp256k1.ec_pubkey_serialize(
-                ctx,
+                secp256k1_ctx,
                 secp256k1_pubkey,
                 pysecp256k1.lib.SECP256K1_EC_COMPRESSED)
 
@@ -117,7 +120,7 @@ class TestPysecp256k1(unittest.TestCase):
 
         # Errors if invalid seralized public key
         with self.assertRaises(ValueError) as err:
-            pysecp256k1.ec_pubkey_parse(ctx, 0)
+            pysecp256k1.ec_pubkey_parse(secp256k1_ctx, 0)
 
         self.assertIn(
             'Invalid pubkey. Must be 33- or 65-bytes.',
@@ -125,16 +128,16 @@ class TestPysecp256k1(unittest.TestCase):
 
         for flags in self.context_flags:
             # Create context
-            ctx = pysecp256k1.context_create(flags)
+            secp256k1_ctx = pysecp256k1.context_create(flags)
 
             # Create UNCOMPRESSED secp256k1_pubkey object to serialize
             secp256k1_pubkey = pysecp256k1.ec_pubkey_parse(
-                ctx,
+                secp256k1_ctx,
                 self.uncomp_pubkey)
 
             # Serialize UNCOMPRESSED pubkey object to byte array pointer
             pubkey_ser = pysecp256k1.ec_pubkey_serialize(
-                ctx,
+                secp256k1_ctx,
                 secp256k1_pubkey,
                 pysecp256k1.lib.SECP256K1_EC_UNCOMPRESSED)
 
@@ -156,8 +159,138 @@ class TestPysecp256k1(unittest.TestCase):
 
         # Errors if invalid seralized public key
         with self.assertRaises(ValueError) as err:
-            pysecp256k1.ec_pubkey_parse(ctx, 0)
+            pysecp256k1.ec_pubkey_parse(secp256k1_ctx, 0)
 
         self.assertIn(
             'Invalid pubkey. Must be 33- or 65-bytes.',
             str(err.exception))
+
+    @unittest.skip('TODO')
+    def test_ecdsa_signature_parse_compact(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ecdsa_signature_parse_der(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ecdsa_signature_serialize_der(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ecdsa_signature_serialize_compact(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ecdsa_verify(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ecdsa_signature_normalize(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ecdsa_sign(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_seckey_verify(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_pubkey_create(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_privkey_negate(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_pubkey_negate(self):
+        pass
+
+    def test_ec_privkey_tweak_add(self):
+        # TODO: Add better testing of resulting tweaked value
+        # Create context
+        secp256k1_ctx = pysecp256k1.context_create(
+            pysecp256k1.lib.SECP256K1_CONTEXT_VERIFY)
+
+        # Create COMPRESSED secp256k1_pubkey object to add tweak
+        secp256k1_pubkey = pysecp256k1.ec_pubkey_parse(
+            secp256k1_ctx,
+            self.pubkey)
+
+        # Tweaked secp256k1_pubkey
+        secp256k1_pubkey_tweak = pysecp256k1.ec_pubkey_tweak_add(
+            secp256k1_ctx,
+            secp256k1_pubkey,
+            self.tweak)
+        #  pubkey_tweak_hex = bytes(
+        #          pysecp256k1.ffi.buffer(secp256k1_pubkey_tweak)).hex()
+
+        # Returns a tweaked secp256k1_pubkey pointer type
+        self.assertEqual(
+            pysecp256k1.ffi.typeof(secp256k1_pubkey_tweak),
+            pysecp256k1.ffi.typeof('secp256k1_pubkey *'))
+
+        # Errors if invalid context
+        with self.assertRaises(TypeError) as err:
+            pysecp256k1.ec_pubkey_tweak_add(0, self.pubkey, self.tweak)
+
+        self.assertIn(
+            'Invalid context. Must be secp256k1_context_struct pointer.',
+            str(err.exception))
+
+        # Errors if invalid pubkey
+        with self.assertRaises(TypeError) as err:
+            pysecp256k1.ec_pubkey_tweak_add(
+                secp256k1_ctx,
+                self.pubkey,
+                self.tweak)
+
+        self.assertIn(
+            'Invalid pubkey. Must be secp256k1_pubkey pointer.',
+            str(err.exception))
+
+        # Errors if invalid tweak
+        with self.assertRaises(ValueError) as err:
+            pysecp256k1.ec_pubkey_tweak_add(
+                secp256k1_ctx,
+                secp256k1_pubkey,
+                bytes.fromhex('00000001'))
+
+        self.assertIn(
+            'Invalid tweak. Must be 32-bytes.',
+            str(err.exception))
+
+    @unittest.skip('TODO')
+    def test_ec_pubkey_tweak_add(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_privkey_tweak_mul(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_pubkey_tweak_mul(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_context_randomize(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_ec_pubkey_combine(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_validate_context(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_valide_public_key(self):
+        pass
+
+    @unittest.skip('TODO')
+    def test_validate_cdata_type(self):
+        pass
