70bde0a9fb914667ac2fc96b6e5860b0e16afa66
James Prestwich <james@prestwi.ch>
2019-01-06 14:54:40 -0800

    correct url in setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     author="RJ Rybarczyk",
     author_email="rj64@protonmail.com",
     description="Python ffi bindings to secp256k1 bitcoin-core library.",
-    url="https://github.com/rrybarczyk/secpy256k1.git",
+    url="https://github.com/summa-tx/secpy256k1",
     packages=setuptools.find_packages(),
     install_requires=['cffi>=1.11.5'],
     cffi_modules=['secpy256k1/build_secp256k1/build.py:ffibuilder'],
